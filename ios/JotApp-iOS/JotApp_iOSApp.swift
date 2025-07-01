f198f8c74fc02686f98fe9791a6f0796d3737483
Daniel Raeder <draeder@gmail.com>
2025-07-01 13:34:50 -0500

    Add data export/import

---
+++ ios/JotApp-iOS/JotApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JotApp_iOSApp.swift
+//  JotApp-iOS
+//
+//  Created by Dan Raeder on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct JotApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
